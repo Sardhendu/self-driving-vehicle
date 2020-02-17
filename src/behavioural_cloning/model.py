e4f5e7aa5eb742be870620c5899c2b18fac410dd
Sardhendu Mishra <sardhendumishra@gmail.com>
2020-02-16 18:54:33 -0600

    Add simple mobilenet

--- src/behavioural_cloning/model.py
+++ src/behavioural_cloning/model.py
@@ -1,8 +1,33 @@
-
+import numpy as np
 import tensorflow as tf
 
-base_model = tf.keras.applications.MobileNetV2(input_shape=(120, 320, 3),
-                                               include_top=False,
-                                               weights='imagenet')
 
-print(base_model.inputs, base_model.outputs)
+class Model:
+    def __init__(self):
+        self.base_model = tf.keras.applications.MobileNetV2(
+                input_shape=(120, 320, 3),
+                include_top=False,
+                weights='imagenet'
+        )
+
+        self.flatten = tf.keras.layers.Flatten()
+        self.dense = tf.keras.layers.Dense(1)
+        
+    def __call__(self):
+        inputs = self.base_model.inputs
+        outputs = self.base_model.outputs
+        outlayer = self.flatten(outputs[0])
+        outlayer = self.dense(outlayer)
+        model = tf.keras.Model(
+                inputs=inputs,
+                outputs=outlayer,
+                name="mobile_net_augmented"
+        )
+        return model
+    
+    
+if __name__ == "__main__":
+    model = Model()
+    out = model()
+    print(out.inputs, out.outputs)
+    
